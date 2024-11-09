8de7c8218a85bc7532daf6dbc49f76268ea4ea21
Lancelot <lancelot@sunset.es>
2024-11-09 18:44:57 -0500

    prime ng added

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,13 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [FloatLabelModule, CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
-
+  value: string | undefined;
 }
